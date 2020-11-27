7415f6876c643795d6c2add76213c57b5b37492a
wood <896615896@example.com>
2020-11-27 18:25:51 +0800

    1

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -1,5 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
+#import <AMapFoundationKit/AMapFoundationKit.h>
+#import <AMapLocationKit/AMapLocationKit.h>
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
